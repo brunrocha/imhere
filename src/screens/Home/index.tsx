d7b91d298fe1f66d52b6215140a5ed2786ac7081
Bruno Rocha <brunorocha-brs@outlook.com>
2022-09-26 16:14:03 -0300

    Removendo participantes

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -19,16 +19,17 @@ export function Home() {
   }
   
   function handleParticipantRemove(name: string) {
+    
+
     Alert.alert("Remover", `Remover o participante ${name}`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert('Deletado!')
+        onPress: () => setParticipants(prevState => prevState.filter(participant => participant !== name))
       },
       {
         text: 'Não'
       }
     ]);
-    console.log(`Você clicou em remover o participante ${name}`);
   }
 
   return (
